d7eb7220ca95e970db15fa29ef114e630290424e
Jyothsna N <jyothsnanattoji@gmail.com>
2023-03-31 17:07:23 +0530

    initial commit

---
+++ StudentRepository/IStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRepo
+{
+    public interface IStudentRepository
+    {
+        bool SearchById(int id);
+        bool SearchByMobile(string mobile);
+        void AddStudent(Student student);
+        List<Student> GetStudentById(int id);
+        List<Student> GetSTudentByName(string name);
+        List<Student> GetAllStudent();
+        Task<List<Student>> GetAllStudentAsync(int pageSize, int pageNumber);
+
+    }
+}
